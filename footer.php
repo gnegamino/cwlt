f1306cdbccf87dcf3595b1843e7684fecbc07c1e
Gian Carlo Egamino <gnegamino@gmail.com>
2018-11-30 16:37:38 +0800

    :fire: Adjusted Spacing and Indentions

--- footer.php
+++ footer.php
@@ -1,79 +1,79 @@
-		<div class="container-fluid">
-			<div id="footer" class="row">
-				<div class="left-part">
-					Copyright @ 2018. CWLT Trucking Services. All Rights Reserved
-				</div>
-				<div class="right-part">
-					<i class="fab fa-facebook fa-2x"></i>
-					<i class="fab fa-instagram fa-2x"></i>
-				</div>
-			</div>
-		</div>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-		<script src="scripts/index.js"></script>
-	</body>
+        <div class="container-fluid">
+            <div id="footer" class="row">
+                <div class="left-part">
+                    Copyright @ 2018. CWLT Trucking Services. All Rights Reserved
+                </div>
+                <div class="right-part">
+                    <i class="fab fa-facebook fa-2x"></i>
+                    <i class="fab fa-instagram fa-2x"></i>
+                </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="scripts/index.js"></script>
+    </body>
 </html>
 <!-- Request a Quote -->
 <div class="modal fade" id="request-quote" tabindex="-1" role="dialog" aria-labelledby="request-quote-label">
-	<div class="modal-dialog" role="document">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title" id="request-quoteLabel">Request a Quote</h4>
-			</div>
-			<div class="modal-body">
-				<form>
-					<div class="row">
-						<div class="col-md-6">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Name">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Company">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Pickup Date">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Pickup Location">
-							</div>
-							<div class="form-group">
-								<select class="form-control">
-									<option>Truck Type / Model</option>
-									<option>Closed Van</option>
-									<option>Big Truck</option>
-								</select>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Email">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Phone">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Pickup Time">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Drop Location">
-							</div>
-							<div class="form-group">
-								<input type="text" class="form-control" placeholder="Type of Cargo">
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						<div class="col-md-12">
-							<textarea class="form-control" rows="4" placeholder="Enter specific instructions, etc."></textarea>
-						</div>
-					</div>
-				</form>
-			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn cwlt-success" data-dismiss="modal">Submit</button>
-			</div>
-		</div>
-	</div>
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="request-quoteLabel">Request a Quote</h4>
+            </div>
+            <div class="modal-body">
+                <form>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Name">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Company">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Pickup Date">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Pickup Location">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control">
+                                    <option>Truck Type / Model</option>
+                                    <option>Closed Van</option>
+                                    <option>Big Truck</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Phone">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Pickup Time">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Drop Location">
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Type of Cargo">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <textarea class="form-control" rows="4" placeholder="Enter specific instructions, etc."></textarea>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn cwlt-success" data-dismiss="modal">Submit</button>
+            </div>
+        </div>
+    </div>
 </div>
--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
-$('body').scrollspy({target: ".navbar", offset: 1220});
+$('body').scrollspy({
+    target: ".navbar",
+    offset: 1220
+});
+
 $('.carousel').carousel();
 
 // Add smooth scrolling on all links inside the navbar
 $("#myNavbar a").on('click', function(event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-  // Make sure this.hash has a value before overriding default behavior
-  if (this.hash !== "") {
+        // Store hash
+        var hash = this.hash;
 
-    // Prevent default anchor click behavior
-    event.preventDefault();
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
 
-    // Store hash
-    var hash = this.hash;
-
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-    $('html, body').animate({
-      scrollTop: $(hash).offset().top
-    }, 800, function(){
-
-    // Add hash (#) to URL when done scrolling (default click behavior)
-      window.location.hash = hash;
-    });
-
-  } // End if
-
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+    }
 });
 
 $(document).scroll(function () {
-  var $nav = $(".navbar-fixed-top");
-  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
